5324f5a314aea3780ee4e827faf6cd2ac5602de4
Yhonatan Peguero <2017-0516@unad.edu.do>
2022-07-07 18:52:23 -0400

    add details component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Search from './components/Search'
+import Details from './components/Details'
 
 function App() {
   return (
@@ -7,9 +8,10 @@ function App() {
       <main className="container mx-auto flex flex-col justify-center p-3 pt-0">
         <Header />
         <Search />
+        <Details />
       </main>
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Details.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Details() {
+  return (
+    <div> 
+      <h1 className="mt-5 text-slate-900">Ropa para hombre</h1>
+      <p className="mt-5 text-gray-600 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere vel explicabo aspernatur molestiae quam totam minus corrupti dolores sequi rerum, unde error ad consequatur ipsam ex! Fugit, veniam earum.</p>
+    </div>
+  );
+}
+
+export default Details;
