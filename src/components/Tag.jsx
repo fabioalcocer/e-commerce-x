4bb2e0ce50f7aa551835735a0fb728a7de9670a8
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-07-12 17:13:41 -0400

    add array for active state

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,7 +1,10 @@
-function Tag({ isActive, text }) {
+import { useState } from "react";
+
+function Tag({ click, isActive, text }) {
 
   return (
     <li
+      onClick={click}
       className={`mr-2 inline-block rounded-3xl bg-gray-800 py-2 px-3 text-white ${
         isActive ? "bg-slate-900 text-white" : "bg-gray-100 text-black"
       }`}
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,15 +1,23 @@
 import Tag from "./Tag";
 
 function Tags() {
+  let states = [false, true, false, false, false, false];
+
+  const handleTag = () => {
+    states = states.map((state) => (state = false));
+    states[3] = true
+    console.log(states);
+  };
+
   return (
     <div className="mt-4">
       <ul className="text-md flex overflow-x-auto text-center font-medium first-letter:text-gray-100">
-        <Tag isActive={true} text="Todo" />
-        <Tag isActive={false} text="Sudaderas" />
-        <Tag isActive={false} text="Camisetas" />
-        <Tag isActive={false} text="Shorts" />
-        <Tag isActive={false} text="Pantalones" />
-        <Tag isActive={false} text="Tenis" />
+        <Tag click={handleTag} isActive={states[0]} text="Todo" />
+        <Tag click={handleTag} isActive={states[1]} text="Sudaderas" />
+        <Tag click={handleTag} isActive={states[2]} text="Camisetas" />
+        <Tag click={handleTag} isActive={states[3]} text="Shorts" />
+        <Tag click={handleTag} isActive={states[4]} text="Pantalones" />
+        <Tag click={handleTag} isActive={states[5]} text="Tenis" />
       </ul>
     </div>
   );
