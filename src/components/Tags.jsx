110f51d73396fccb0d7143ddb805aae1a5d8cb50
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-27 23:08:24 -0400

    refactor: change the logic to tags component

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -2,21 +2,14 @@ import { useContext, useState } from 'react'
 import { AppContext } from '../Context'
 
 function Tags () {
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState('Todo')
   const { filterProducts } = useContext(AppContext)
 
-  const tags = [
-    { id: 1, text: 'Todo' },
-    { id: 2, text: 'Sudaderas' },
-    { id: 3, text: 'Poleras' },
-    { id: 4, text: 'Shorts' },
-    { id: 5, text: 'Pantalones' },
-    { id: 6, text: 'Tennis' }
-  ]
+  const tags = ['Todo', 'Sudaderas', 'Poleras', 'Shorts', 'Pantalones', 'Tennis']
 
   const handleTags = (e, tag) => {
-    setActive(tag.id)
     filterProducts(e)
+    setActive(tag)
   }
 
   return (
@@ -24,15 +17,15 @@ function Tags () {
       <ul className='text-md flex overflow-x-auto p-1 pl-[2px] text-center font-medium text-gray-100'>
         {tags.map((tag) => (
           <li
-            key={tag.id}
+            key={tag}
             onClick={(e) => handleTags(e, tag)}
             className={`tag mr-2 inline-block rounded-3xl bg-gray-800 py-2 px-3 text-white ${
-              active === tag.id
+              active === tag
                 ? 'bg-slate-900 text-white  dark:outline dark:outline-2 dark:outline-slate-50'
                 : 'bg-gray-200 bg-opacity-70 text-black dark:bg-gray-100'
             } cursor-pointer`}
           >
-            {tag.text}
+            {tag}
           </li>
         ))}
       </ul>
