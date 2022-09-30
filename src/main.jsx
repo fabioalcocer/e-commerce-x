4c7ff6fb29688dda7be3f205bc2e1712c5c358ba
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-30 17:40:51 -0400

    add partytown

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AppContextProvider } from './Context'
+import Head from './services/partytown'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContextProvider>
+      <Head />
       <App />
     </AppContextProvider>
   </React.StrictMode>
