25b2bb6a977d103c16e12df3a8848d538fa5e0e8
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-30 17:46:15 -0400

    Revert "add partytown"
    This reverts commit 4c7ff6fb29688dda7be3f205bc2e1712c5c358ba.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AppContextProvider } from './Context'
-import Head from './services/partytown'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppContextProvider>
-      <Head />
       <App />
     </AppContextProvider>
   </React.StrictMode>
