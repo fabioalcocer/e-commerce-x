340595dc7537cabd51581c057e6b0a5d948a7b82
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-10-22 15:08:49 -0400

    update preview to README

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,17 +1,18 @@
 function Details () {
   return (
     <section className='details flex flex-col items-center pr-2 pl-[3px] text-center'>
-      <h1 className='mt-9 text-4xl md:text-[2.5rem] text-slate-900 dark:text-gray-200'>
+      <h1 className='mt-9 text-4xl text-slate-900 dark:text-gray-200 md:text-[2.5rem]'>
         El Chiringuito
       </h1>
-      <p className='mt-5 mb-5 max-w-3xl px-0 md:px-4 text-sm md:leading-7 text-gray-900 dark:text-slate-200 md:text-base'>
-        Tienda de ropa Deportiva desarrollada en unos dias, que aun no
-        tiene una descripcion concreta, por lo cual no puedo definir
-        un texto para la susodicha, lo que me lleva a rellenar este
-        campo con boludeces sin sentido, pero que para suerte de
-        muchos, nadie tendra que leer porque esto no lo vera nadie
-        entonces todo tranqui, y todo god. Y total soy re nazi asi que
-        eso.
+      <p className='mt-5 mb-5 max-w-3xl px-0 text-sm text-gray-900 dark:text-slate-200 md:px-4 md:text-base md:leading-7'>
+        Tienda de ropa Deportiva desarrollada Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Adipisci quibusdam nam
+        consectetur! Quos, qui voluptatum modi recusandae ea
+        consequuntur alias, ipsum cupiditate eius explicabo odit
+        dolorum, labore maxime consequatur blanditiis. eso. Lorem
+        ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
+        quod nihil? Illo, voluptate perferendis vel culpa fugiat
+        dignissimos sequi est magnam reprehenderit.
       </p>
     </section>
   )
